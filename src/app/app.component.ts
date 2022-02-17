0b503081cf6c82630eeafe17c3d5d0b7d0711e6a
Srinath S <srinath@Srinaths-MacBook-Air.local>
2022-02-17 14:20:11 +0530

    Added : HelloWorld Project Using AngularJS

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,29 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+
   title = 'Hello_World';
+  imgUrl = "../assets/BridgeLabz_Logo.svg";
+  url = "https://www.bridgelabz.com/";
+  userName : string = "";
+  nameError : string = "";
+
+  ngOnInit(): void {
+    this.title = "Hello from Bridgelabz";
+  }
+
+  onClick($event: any){
+    console.log("Save Button is Clicked" , $event);
+    window.open(this.url, "_blank");
+  }
+
+  onInput($event: any){
+    console.log("Change Event Occured", $event.data);
+    const nameRegex = RegExp('^[A-Z]{1}[a-zA-Z\\s]{2,}$');
+    if (nameRegex.test(this.userName)){
+      this.nameError = "";
+      return;
+    }
+    this.nameError = "Name is Incorrect"
+  }
 }
